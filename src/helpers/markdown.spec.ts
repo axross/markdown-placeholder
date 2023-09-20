07f6b30c765aa01473fbf3a730266f95fa96a101
Kohei Asai <yo@kohei.dev>
2023-09-19 22:58:03 -0700

    feat: strong node function

--- src/helpers/markdown.spec.ts
+++ src/helpers/markdown.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { createRand, randText } from "./markdown";
+import { createRand, randStrongEmphasis, randText } from "./markdown";
 
 describe("randText()", () => {
   it("returns a text node with random string value", () => {
@@ -32,6 +32,62 @@ describe("randText()", () => {
       {
         "type": "text",
         "value": "excepteur consectetur labore",
+      }
+    `);
+  });
+});
+
+describe("randStrongEmphasis()", () => {
+  it("returns a text node with random string value", () => {
+    const rand = createRand();
+
+    expect(randStrongEmphasis({ rand })).toMatchInlineSnapshot(`
+      {
+        "children": [
+          {
+            "type": "text",
+            "value": "dolore veniam",
+          },
+        ],
+        "type": "strong",
+      }
+    `);
+  });
+
+  it("continuously returns text nodes with random string value when you call it more than once", () => {
+    const rand = createRand();
+
+    expect(randStrongEmphasis({ rand })).toMatchInlineSnapshot(`
+      {
+        "children": [
+          {
+            "type": "text",
+            "value": "dolore veniam",
+          },
+        ],
+        "type": "strong",
+      }
+    `);
+    expect(randStrongEmphasis({ rand })).toMatchInlineSnapshot(`
+      {
+        "children": [
+          {
+            "type": "text",
+            "value": "ullamco",
+          },
+        ],
+        "type": "strong",
+      }
+    `);
+    expect(randStrongEmphasis({ rand })).toMatchInlineSnapshot(`
+      {
+        "children": [
+          {
+            "type": "text",
+            "value": "sint",
+          },
+        ],
+        "type": "strong",
       }
     `);
   });
