fe243a7822f5e5eb96d8eb88b0c354d41f6089cd
Kohei Asai <yo@kohei.dev>
2023-09-20 21:59:25 -0700

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,4 +2,12 @@
 
 module.exports = {
   extends: ["@axross/eslint-config", "next/core-web-vitals"],
+  overrides: [
+    {
+      files: ["*.@(js|mjs|cjs|ts|mts|cts)"],
+      rules: {
+        "no-magic-numbers": ["error", { ignore: [0, 1] }],
+      },
+    },
+  ],
 };
