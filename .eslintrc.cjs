3768322588e18084727b1138ef720f85070ad8cd
Kohei <yo@kohei.dev>
2023-09-23 11:47:33 -0700

    chore: refactoring paragraph function (#27)
    * chore: refactoring paragraph function
    
    * chore: delete unnecessary files
    
    * chore: fix lint issues
    
    * chore(lint): update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,24 @@ module.exports = {
     {
       files: ["*.@(js|mjs|cjs|ts|mts|cts)"],
       rules: {
-        "no-magic-numbers": ["error", { ignore: [0, 1] }],
+        "@typescript-eslint/no-magic-numbers": ["error", { ignore: [0, 1] }],
+      },
+    },
+    {
+      files: ["*.@(js|mjs|cjs|jsx|ts|mts|cts|tsx)"],
+      extends: ["plugin:unicorn/recommended"],
+      rules: {
+        "unicorn/numeric-separators-style": "off",
+      },
+    },
+    {
+      files: [
+        "*rc.@(js|mjs|cjs|ts|mts|cts|json)",
+        "*.config.@(js|mjs|cjs|ts|mts|cts|json)",
+      ],
+      plugins: ["eslint-plugin-unicorn"],
+      rules: {
+        "unicorn/prefer-module": "off",
       },
     },
   ],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { type JSX } from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
